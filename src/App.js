fc7bc2f6c11c56c3a517b9ee9c142e8fb49d910e
Ahmed Rizk <ahmed.rezk.fe@gmail.com>
2020-11-09 19:14:03 +0200

    v 3 from app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+// import component from react
 import { Switch, Route } from "react-router-dom";
 
+// import my page 
 import HomePage from "./Pages/HomePage/homePage.Component";
 import ShopPage from "./Pages/Shop/Shop.component";
 
 // import header
 import Header from "./Components/Component_Header/Header/Header.component";
 
+// import css 
 import "./App.css";
 
 function App() {
